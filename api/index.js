23d44a507e044f163bb265f13c136d252ad7fb88
chandan0303 <ahaakmisra@gmail.com>
2024-10-14 11:34:48 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -6,6 +6,8 @@ import authRouter from "./routes/auth.route.js"
 import listingRouter from "./routes/listing.route.js"
 import cookieParser from "cookie-parser"
 
+import path from "path"
+
 dotenv.config()
 
 
@@ -18,6 +20,8 @@ mongoose
     console.log(err);
   });
 
+  const __dirname=path.resolve()
+
 const app=express()
 app.use(express.json())
 app.use(cookieParser())
@@ -29,6 +33,12 @@ app.listen(3000,()=>{
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing",listingRouter)
+
+app.use(express.static(path.join(__dirname,"/client/dist")))
+
+app.get("*",(req,res)=>{
+  res.sendFile(path.join(__dirname,"client","dist","index.html"))
+})
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
