d9766fb5a9c8ab3fde1070cf69bb03adb35fe111
chandan0303 <ahaakmisra@gmail.com>
2025-02-10 18:22:17 +0530

    final commit

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import lineClamp from '@tailwindcss/line-clamp';
+
 export default {
   content: [
     "./index.html",
@@ -8,7 +10,24 @@ export default {
     extend: {},
   },
   plugins: [
-    require('@tailwindcss/line-clamp'),
-    // ...
+    lineClamp, // Use the imported plugin
   ],
-}
+};
+
+
+
+
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,ts,jsx,tsx}",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [
+//     require('@tailwindcss/line-clamp'),
+//     // ...
+//   ],
+// }
